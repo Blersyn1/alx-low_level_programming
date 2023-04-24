98ced863e3b3db3476f3b073ea3ead5457585ee8
blersyn1 <blersynottah258@gmail.com>
2023-04-24 02:27:01 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main - write a C program using puts function
+/**
+ * main -Write a C program using puts function
  *
  * Return: 0
 */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
